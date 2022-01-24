97955bafbe362b771d14cbec2ebcfeb1a4c79ba7
emilio kariuki <70817006+emilio-kariuki@users.noreply.github.com>
2022-01-24 17:21:17 +0300

    commit

--- lib/Navi.dart
+++ lib/Navi.dart
@@ -2,6 +2,7 @@ import "package:flutter/material.dart";
 import 'Accounts.dart';
 import 'Post.dart';
 import 'Home.dart';
+import 'item.dart';
 
 class Navi extends StatefulWidget {
   Navi({Key? key}) : super(key: key);
@@ -14,6 +15,7 @@ class _NaviState extends State<Navi> {
   int selectedIndex = 0;
   List<Widget> pages = [
     Home(),
+    Item(),
     Post(),
     Accounts(),
   ];
@@ -33,14 +35,42 @@ class _NaviState extends State<Navi> {
       body: Center(
         child: pages.elementAt(selectedIndex),
       ),
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: selectedIndex,
-        onTap: onItemTap,
-        items: [
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
-          BottomNavigationBarItem(icon: Icon(Icons.upload), label: "Post"),
-          BottomNavigationBarItem(icon: Icon(Icons.person), label: "Accounts"),
-        ],
+      bottomNavigationBar: Padding(
+        padding: EdgeInsets.fromLTRB(3, 0, 3, 0),
+        child: Card(
+            color: Colors.green[800],
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(20),
+            ),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: BottomNavigationBar(
+                selectedItemColor: Colors.white,
+                unselectedItemColor: Colors.black,
+                type: BottomNavigationBarType.fixed,
+                backgroundColor: Colors.green[800],
+                currentIndex: selectedIndex,
+                onTap: onItemTap,
+                // ignore: prefer_const_literals_to_create_immutables
+                items: [
+                  BottomNavigationBarItem(
+                      icon: Icon(
+                        Icons.home,
+                      ),
+                      label: "Home"),
+                  BottomNavigationBarItem(
+                      icon: Icon(
+                        Icons.layers
+                      ),
+                      label: "Item"),
+                  BottomNavigationBarItem(
+                      icon: Icon(Icons.upload), label: "Post"),
+                  // BottomNavigationBarItem(icon: Icon(Icons.layers), label: "Item"),
+                  BottomNavigationBarItem(
+                      icon: Icon(Icons.person), label: "Accounts"),
+                ],
+              ),
+            )),
       ),
     );
   }
--- lib/Post.dart
+++ lib/Post.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: deprecated_member_use
+
 import 'package:flutter/material.dart';
 
 class Post extends StatefulWidget {
@@ -8,10 +10,22 @@ class Post extends StatefulWidget {
 }
 
 class _PostState extends State<Post> {
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text("Emilio post"))
+      body: Center(
+        child: Card(
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),color: Colors.purple,
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(30, 1, 30, 1),
+            child: FlatButton(
+              color: Colors.purple,
+              onPressed: () {},
+              child: Text("Get location")),
+          )
+        ),
+      )
     );
   }
 }
